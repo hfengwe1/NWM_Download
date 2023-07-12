99df258b20ce6072304a60fe9e4dbcb307354654
Fengwei Hung <fwhung0807@gmail.com>
2023-07-12 16:20:17 -0400

    commit message"

---
+++ NWM_Download_v1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os
+import numpy as np
+import pandas as pd
+import xarray as xy 
+
+os.getcwd()
+os.chdir('C:\\NWM')
+
